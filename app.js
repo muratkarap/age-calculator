f13a5ccfa62ccc17fa39e30a19efd1fc8647d525
muratkarap <mkarap1978@gmail.com>
2022-02-10 10:29:00 +0100

    step3

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 let year_birth=document.getElementById("birthday");
-console.log(year_birth);
+console.log(year_birth.value);
+let years_calculate=document.getElementsByClassName("table_years")[0];
+console.log(years_calculate.innerText);
+year_birth.addEventListener("click",()=>{
+    let year=new Date().getFullYear()-new Date(year_birth.value).getFullYear();
+years_calculate.addEventListener("change",()=>{
+    years_calculate+=year;
+})
+})
